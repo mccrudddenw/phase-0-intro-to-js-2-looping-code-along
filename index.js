2155acbe8289e5e3a1951f90d990f060fb6dfef5
mccruddenw <wyatt.mccrudden@gmail.com>
2021-03-15 11:48:56 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+
+
+const names = ["Lisa", "Kaitlin", "Jan"];
+
+function writeCards(names)  {
+
+    let messages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+       
+    }
+    return messages;
+}
+
+
+function countDown() {
+ 
+    let count = 10;
+    while (count > -1) {
+      console.log(count--);
+    }
+      
+    }
+    
